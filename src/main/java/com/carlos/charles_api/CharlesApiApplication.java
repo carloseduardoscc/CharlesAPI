6b92ff774ced71a114e0187dc3cb820a789fec19
Carlos <carloseduscc@gmail.com>
2025-03-12 19:47:49 -0300

    Project created

---
+++ src/main/java/com/carlos/charles_api/CharlesApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.charles_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharlesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharlesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/charles_api/CharlesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.charles_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharlesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
